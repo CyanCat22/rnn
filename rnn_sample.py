aa896740a4cb421517c8fb8aedf134932097df52
guagua <1677574244@qq.com>
2023-10-25 23:26:01 +0800

    run

--- rnn_sample.py
+++ rnn_sample.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn 
 from torch.nn import functional as F
 from d2l import torch as d2l
+
 """
 rnn参数共享
 h=a(wh+wh+b)
@@ -30,6 +31,43 @@ class RNNModel(nn.Module):
             self.num_directions = 1
             # 构造自己的输出层
             self.linear = nn.Linear(self.num_hiddens, self.vocab_size)
+        else:
+            self.num_directions = 2
+            self.linear = nn.Linear(self.num_hiddens * 2, self.vocab_size)
+
+    def forward(self, inputs, state):
+        X = F.one_hot(inputs.T.long(), self.vocab_size)
+        X = X.to(torch.float32)
+        Y, state = self.rnn(X, state)
+        # 全连接层首先将Y的形状改为(时间步数*批量大小,隐藏单元数)
+        # 它的输出形状是(时间步数*批量大小,词表大小)。
+        output = self.linear(Y.reshape((-1, Y.shape[-1])))
+        return output, state
+
+    def begin_state(self, device, batch_size=1):
+        if not isinstance(self.rnn, nn.LSTM):
+            # nn.GRU以张量为隐状态
+            return torch.zeros((self.num_directions * self.rnn.num_layers, 
+                                batch_size, self.num_hiddens),
+                                device=device)
+            # 样本特征维度
+        else:
+            # nn.LSTM以元组作为隐状态
+            return (torch.zeros((
+                self.num_directions * self.rnn.num_layers,
+                batch_size, self.num_hiddens), device=device),
+                    torch.zeros((
+                        self.num_directions * self.rnn.num_layers,
+                        batch_size, self.num_hiddens), device=device))
         
+        
+device = d2l.try_gpu()    
+net = RNNModel(rnn_layer, vocab_size=len(vocab))
+net = net.to(device)
+d2l.predict_ch8('time traveller', 10, net, vocab, device)
+
+num_epochs, lr = 500, 1
+d2l.train_ch8(net, train_iter, vocab, lr, num_epochs, device)
+            
 
 
