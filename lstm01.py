48730fdfb8dd200fc7f08bc4ddc09a8c09056d12
guagua <1677574244@qq.com>
2023-10-29 14:44:39 +0800

    'lululu'

--- lstm01.py
+++ lstm01.py
@@ -1,5 +1,16 @@
 import torch
 from torch import nn
 from d2l import torch as d2l
+batch_size, num_steps = 32, 35
+train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
+
+num_inputs = len(vocab)
+num_hiddens = 256
+device = d2l.try_gpu()
+num_epochs, lr = 500, 1
+lstm_layer = nn.LSTM(num_inputs, num_hiddens)
+model = d2l.RNNModel(lstm_layer, len(vocab))
+model = model.to(device)
+d2l.train_ch8(model, train_iter, vocab, lr, num_epochs, device)
 
 
